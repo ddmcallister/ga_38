89a6b6faf70a4985dbe59b07ae89c308fc44be04
ddmcallister <destanie@gmail.com>
2014-09-23 11:58:07 -0400

    lights work! but script too specific

--- GA_2b_traffic_light_js/JS/application.js
+++ GA_2b_traffic_light_js/JS/application.js
@@ -9,20 +9,12 @@ var lightsOff = document.getElementsByClassName("lights");
 	var k;
 	var arrLights = [];
     for(var k = lightsOff.length; k--; arrLights.unshift(lightsOff[k])); {
+    	if (arrLights[0].id.toLowerCase(0) == lightOn) {
+    	arrLights[0].style.backgroundColor = lightOn;
+    } else if (arrLights[1].id.toLowerCase(1) == lightOn) {
+    	arrLights[1].style.backgroundColor = lightOn;
+    }	else {
+    	arrLights[2].style.backgroundColor = lightOn;
     }
-
-var lightOn = document.getElementsByTagName("button");
-	var j;
-    var arrButtons = [];
-    for(var j = lightOn.length; j--; arrButtons.unshift(lightOn[j])); {
-	}
-
-var oneLight;
-	if (arrLights[0].id.toLowerCase() == lightOn) {
-    		arrLights[0].style.backgroundColor = lightOn;
     }
-
-	
-console.log(arrLights);
-console.log(arrButtons);
 }
