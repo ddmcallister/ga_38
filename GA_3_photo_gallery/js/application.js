f17a5bdc88eff71e0bcdbaa5e375ea351dc255cc
ddmcallister <destanie@gmail.com>
2014-10-01 23:56:21 -0400

    stuck on forward/back buttons

--- GA_3_photo_gallery/js/application.js
+++ GA_3_photo_gallery/js/application.js
@@ -1,18 +1,46 @@
 var picClick = function(thumbnailCenter) {
-	var goal = document.getElementById("mainImg");
 	var startPos = document.getElementsByClassName("thumbnail");
 	var i;
 	for (i=0; i<startPos.length; i++) {	
 		startPos[i].style.opacity = 1;
 		}
-
-var k;
+	var k;
 	var arrThumbnail = []
-	for (var k = startPos.length; k--; arrThumbnail.push(startPos[k])); {
+	for (var k = startPos.length; k--; arrThumbnail.unshift(startPos[k])); {
+		var goal = document.getElementById("mainImg");
 		goal.src = arrThumbnail[thumbnailCenter].src;
 		arrThumbnail[thumbnailCenter].style.opacity = 0.5;
-
 	}
+}
 
-	
-	}
+var currentImage = 0;
+
+function forClick(target) {
+	var startPos = document.getElementsByClassName("thumbnail");
+	var z;
+	var arrThumbnail = []
+	for (var z = startPos.length; z--; arrThumbnail.unshift(startPos[z])); {
+		if(currentImage == arrThumbnail.length--){
+     		currentImage = 0;
+            document.getElementById("mainImg").src = arrThumbnail[currentImage].src;
+        } else {
+            currentImage ++;
+            document.getElementById("mainImg").src = arrThumbnail[currentImage].src;
+        }
+    }
+}
+
+function backClick() {
+	var startPos = document.getElementsByClassName("thumbnail");
+	var q;
+	var arrThumbnail = []
+	for (var q = startPos.length; q--; arrThumbnail.unshift(startPos[q])); {
+		if(currentImage == 0) {
+     		currentImage = arrThumbnail.length;
+            document.getElementById("mainImg").src = arrThumbnail[currentImage].src;
+        } else {
+            currentImage--;
+            document.getElementById("mainImg").src = arrThumbnail[currentImage].src;
+        }
+    }
+}
