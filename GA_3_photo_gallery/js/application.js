e815c401609069a7130f4535efe485e3c4645d6e
ddmcallister <destanie@gmail.com>
2014-10-02 15:18:53 -0400

    photo gallery functional but ugly

--- GA_3_photo_gallery/js/application.js
+++ GA_3_photo_gallery/js/application.js
@@ -4,43 +4,55 @@ var picClick = function(thumbnailCenter) {
 	for (i=0; i<startPos.length; i++) {	
 		startPos[i].style.opacity = 1;
 		}
-	var k;
-	var arrThumbnail = []
-	for (var k = startPos.length; k--; arrThumbnail.unshift(startPos[k])); {
 		var goal = document.getElementById("mainImg");
-		goal.src = arrThumbnail[thumbnailCenter].src;
+		goal.src = "///Users/Destanie/Desktop/GA/GA_3_photo_gallery/img/" + thumbnailCenter + ".jpg";
 		arrThumbnail[thumbnailCenter].style.opacity = 0.5;
 	}
-}
 
-var currentImage = 0;
+function forClick() {
+		var foo = document.getElementById('mainImg');
+		var startPos = document.getElementsByClassName("thumbnail");
+		var z;
+		var arrThumbnail = []
+		for (var z = startPos.length; z--; arrThumbnail.unshift(startPos[z])); {
+			var i = foo.src;
+			var ba = i.replace(/^.*[\\\/]/, '');
+			var r = ba.replace(".jpg", '');
+			if(r == 23){
+     				r = 0;
+     				var img = document.getElementById('mainImg');
+     				img.src = '///Users/Destanie/Desktop/GA/GA_3_photo_gallery/img/0.jpg';
+     			}
+			else {
+					r++;
+					newVar = "///Users/Destanie/Desktop/GA/GA_3_photo_gallery/img/" + r + ".jpg";
+					var img = document.getElementById('mainImg');
+					img.src = newVar;}
+     			}
+	}
 
-function forClick(target) {
-	var startPos = document.getElementsByClassName("thumbnail");
-	var z;
-	var arrThumbnail = []
-	for (var z = startPos.length; z--; arrThumbnail.unshift(startPos[z])); {
-		if(currentImage == arrThumbnail.length--){
-     		currentImage = 0;
-            document.getElementById("mainImg").src = arrThumbnail[currentImage].src;
-        } else {
-            currentImage ++;
-            document.getElementById("mainImg").src = arrThumbnail[currentImage].src;
-        }
+	function backClick() {
+		var foo = document.getElementById('mainImg');
+		var startPos = document.getElementsByClassName("thumbnail");
+		var z;
+		var arrThumbnail = []
+		for (var z = startPos.length; z--; arrThumbnail.unshift(startPos[z])); {
+			var i = foo.src;
+			var ba = i.replace(/^.*[\\\/]/, '');
+			console.log(ba);
+			var r = ba.replace(".jpg", '');
+			console.log(r);
+			if(r == 0){
+     				r = 23;
+     				var img = document.getElementById('mainImg');
+     				img.src = '///Users/Destanie/Desktop/GA/GA_3_photo_gallery/img/23.jpg';
+     			}
+			else {
+					r--;
+					newVar = "///Users/Destanie/Desktop/GA/GA_3_photo_gallery/img/" + r + ".jpg";
+					var img = document.getElementById('mainImg');
+					img.src = newVar;
+				}
     }
 }
 
-function backClick() {
-	var startPos = document.getElementsByClassName("thumbnail");
-	var q;
-	var arrThumbnail = []
-	for (var q = startPos.length; q--; arrThumbnail.unshift(startPos[q])); {
-		if(currentImage == 0) {
-     		currentImage = arrThumbnail.length;
-            document.getElementById("mainImg").src = arrThumbnail[currentImage].src;
-        } else {
-            currentImage--;
-            document.getElementById("mainImg").src = arrThumbnail[currentImage].src;
-        }
-    }
-}
