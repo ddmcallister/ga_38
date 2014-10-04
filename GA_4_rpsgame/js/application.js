d76c1f3435447d6bc2f89dc61a07eda9b9897ae4
ddmcallister <destanie@gmail.com>
2014-10-04 17:01:46 -0400

    kitten compare doesn't work

--- GA_4_rpsgame/js/application.js
+++ GA_4_rpsgame/js/application.js
@@ -1,10 +1,3 @@
-var i = Math.floor(Math.random() * 10);
-if(i==0) {
-	var x = Math.floor(Math.random() * 10);
-	i = x;
-}
-console.log(i);
-
 /*var compChoice = function() {
 	if (i<=3) {
 		compChoice = "rock";
@@ -14,21 +7,30 @@ console.log(i);
 		compChoice = "scissors";
 	}
 }*/
+/*var plRock = function(rock) {
+	var i = 0;
+	i = Math.floor(Math.random() * 10);
+	if(i==0) {
+		var x = Math.floor(Math.random() * 10);
+		i = x;
+	}
 
-var plRock = function(rock) {
 	if(i<=3) {
+		return tieResponse;
 		var tieResponse = prompt("Tie! Would you like to record your score?");
 			if (tieResponse == "yes") {
   				document.getElementById("tieCount").innerHTML=
   				parseInt(document.getElementById("tieCount").innerHTML,10)+1;
 			}
 	} else if(i<=6) {
+		return loseResponse;
 		var loseResponse = prompt("Sorry, you lose! Would you like to record your score?");
 			if (loseResponse == "yes") {
   				document.getElementById("loseCount").innerHTML=
   				parseInt(document.getElementById("loseCount").innerHTML,10)+1;
 			}
 	} else if(i<=9) {
+		return winResponse;
 		var winResponse = prompt("You win! Would you like to record your score?");
 			if (winResponse == "yes") {
   				document.getElementById("winCount").innerHTML=
@@ -38,22 +40,123 @@ var plRock = function(rock) {
 	}
 
 var plPaper = function(rock) {
+	var i = 0;
+	i = Math.floor(Math.random() * 10);
+	if(i==0) {
+		var x = Math.floor(Math.random() * 10);
+		i = x;
+	}
+
 	if(i<=3) {
-		prompt("You win! Would you like to record your score?");
+		var winResponse = prompt("You win! Would you like to record your score?");
+		if (winResponse == "yes") {
+  				document.getElementById("winCount").innerHTML=
+  				parseInt(document.getElementById("winCount").innerHTML,10)+1;
+			}
 	} else if(i<=6) {
-		prompt("Tie! Would you like to record your score?");
+		var tieResponse = prompt("Tie! Would you like to record your score?");
+		if (tieResponse == "yes") {
+  				document.getElementById("tieCount").innerHTML=
+  				parseInt(document.getElementById("tieCount").innerHTML,10)+1;
+			}
 	} else if(i<=9) {
-		prompt("Sorry, you lose! Would you like to record your score?");
+		var loseResponse = prompt("Sorry, you lose! Would you like to record your score?");
+		if (loseResponse == "yes") {
+  				document.getElementById("loseCount").innerHTML=
+  				parseInt(document.getElementById("loseCount").innerHTML,10)+1;
+			}
 	}
 }
 
 var plScissors = function(rock) {
-	if(i<=3) {
-		prompt("Sorry, you lose! Would you like to record your score?");
-	} else if(i<=6) {
-		prompt("You win! Would you like to record your score?");
-	} else if(i<=9) {
-		prompt("Tie! Would you like to record your score?");
+	var i = 0;
+	i = Math.floor(Math.random() * 10);
+	if(i==0) {
+		var x = Math.floor(Math.random() * 10);
+		i = x;
 	}
+	if(i<=3) {
+		var loseResponse = prompt("Sorry, you lose! Would you like to record your score?");
+		if (loseResponse == "yes") {
+  				document.getElementById("loseCount").innerHTML=
+  				parseInt(document.getElementById("loseCount").innerHTML,10)+1;
+			}
+	} else if(i<=6) {
+		var winResponse = prompt("You win! Would you like to record your score?");
+		if (winResponse == "yes") {
+  				document.getElementById("winCount").innerHTML=
+  				parseInt(document.getElementById("winCount").innerHTML,10)+1;
+			}
+	} else if(i<=9) {
+		var tieResponse = prompt("Tie! Would you like to record your score?");
+		if (tieResponse == "yes") {
+  				document.getElementById("tieCount").innerHTML=
+  				parseInt(document.getElementById("tieCount").innerHTML,10)+1;
+			}
+	}
+}*/
+
+//here begins kitten mode
+
+var kittenMode = function() {
+	var plRock = 0;
+	var plpaper = 0;
+	var plScissors = 0;
+
+	$(document).ready(function(){
+		console.log(plRock);
+
+	});
+
+	$(document).keydown(function(event) {
+		var i = 0;
+		var z=0;
+		
+		i = Math.floor(Math.random() * 10);
+		if(i==0) {
+			var x = Math.floor(Math.random() * 10);
+			i = x;	
+		}
+		var plRandomChoice = i;
+
+		z = Math.floor(Math.random() * 10);
+		if(z==0) {
+		var y = Math.floor(Math.random() * 10);
+		z = y;
+		}
+		var compChoice = z;
+
+		var scoreCompare = function() {	
+
+			if (plRandomChoice>compChoice) {
+				return winResponse;
+				var winResponse = prompt("You win! Would you like to record your score?");
+				if (winResponse == "yes") {
+  					document.getElementById("winCount").innerHTML=
+  					parseInt(document.getElementById("winCount").innerHTML,10)+1;
+				}
+		}
+		else if (plRandomChoice===compChoice) {
+			return tieResponse;
+			var tieResponse = prompt("Tie! Would you like to record your score?");
+				if (tieResponse == "yes") {
+  					document.getElementById("tieCount").innerHTML=
+  					parseInt(document.getElementById("tieCount").innerHTML,10)+1;
+				} 
+		}
+		else if (plRandomChoice<compChoice) {
+			return loseResponse;
+			var loseResponse = prompt("Sorry, you lose! Would you like to record your score?");
+				if (loseResponse == "yes") {
+  					document.getElementById("loseCount").innerHTML=
+  					parseInt(document.getElementById("loseCount").innerHTML,10)+1;
+				}
+		}
+	}	
+
+scoreCompare();
+
+});
+
 }
 
