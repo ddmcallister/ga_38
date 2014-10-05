f2f5e667aa83590d0349b7870d4d4bfd35090cbb
ddmcallister <destanie@gmail.com>
2014-10-05 17:22:50 -0400

    mostly done, looks funny at narrow widths

--- GA_4_rpsgame/js/application.js
+++ GA_4_rpsgame/js/application.js
@@ -7,7 +7,7 @@
 		compChoice = "scissors";
 	}
 }*/
-/*var plRock = function(rock) {
+var plRock = function() {
 	var i = 0;
 	i = Math.floor(Math.random() * 10);
 	if(i==0) {
@@ -16,30 +16,28 @@
 	}
 
 	if(i<=3) {
-		return tieResponse;
 		var tieResponse = prompt("Tie! Would you like to record your score?");
 			if (tieResponse == "yes") {
   				document.getElementById("tieCount").innerHTML=
   				parseInt(document.getElementById("tieCount").innerHTML,10)+1;
 			}
 	} else if(i<=6) {
-		return loseResponse;
 		var loseResponse = prompt("Sorry, you lose! Would you like to record your score?");
 			if (loseResponse == "yes") {
   				document.getElementById("loseCount").innerHTML=
   				parseInt(document.getElementById("loseCount").innerHTML,10)+1;
 			}
 	} else if(i<=9) {
-		return winResponse;
 		var winResponse = prompt("You win! Would you like to record your score?");
 			if (winResponse == "yes") {
   				document.getElementById("winCount").innerHTML=
   				parseInt(document.getElementById("winCount").innerHTML,10)+1;
 			}
-	}
+		}
 	}
 
-var plPaper = function(rock) {
+
+var plPaper = function(paper) {
 	var i = 0;
 	i = Math.floor(Math.random() * 10);
 	if(i==0) {
@@ -68,7 +66,7 @@ var plPaper = function(rock) {
 	}
 }
 
-var plScissors = function(rock) {
+var plScissors = function(scissors) {
 	var i = 0;
 	i = Math.floor(Math.random() * 10);
 	if(i==0) {
@@ -94,7 +92,7 @@ var plScissors = function(rock) {
   				parseInt(document.getElementById("tieCount").innerHTML,10)+1;
 			}
 	}
-}*/
+}
 
 //here begins kitten mode
 
@@ -102,6 +100,8 @@ var kittenMode = function() {
 	var plRock = 0;
 	var plpaper = 0;
 	var plScissors = 0;
+
+	document.getElementById("kitten_button").style.opacity = 1;
 
 	$(document).ready(function(){
 		console.log(plRock);
@@ -111,7 +111,12 @@ var kittenMode = function() {
 	$(document).keydown(function(event) {
 		var i = 0;
 		var z=0;
-		
+
+    	var code = (event.keyCode || event.which);
+    	if(code == 13) {
+        return;
+    	}
+
 		i = Math.floor(Math.random() * 10);
 		if(i==0) {
 			var x = Math.floor(Math.random() * 10);
@@ -129,24 +134,22 @@ var kittenMode = function() {
 		var scoreCompare = function() {	
 
 			if (plRandomChoice>compChoice) {
-				return winResponse;
-				var winResponse = prompt("You win! Would you like to record your score?");
+
+				var winResponse = prompt("You win! Record your score?");
 				if (winResponse == "yes") {
   					document.getElementById("winCount").innerHTML=
   					parseInt(document.getElementById("winCount").innerHTML,10)+1;
 				}
 		}
 		else if (plRandomChoice===compChoice) {
-			return tieResponse;
-			var tieResponse = prompt("Tie! Would you like to record your score?");
+			var tieResponse = prompt("Tie! Record your score?");
 				if (tieResponse == "yes") {
   					document.getElementById("tieCount").innerHTML=
   					parseInt(document.getElementById("tieCount").innerHTML,10)+1;
 				} 
 		}
 		else if (plRandomChoice<compChoice) {
-			return loseResponse;
-			var loseResponse = prompt("Sorry, you lose! Would you like to record your score?");
+			var loseResponse = prompt("You lose! Record your score?");
 				if (loseResponse == "yes") {
   					document.getElementById("loseCount").innerHTML=
   					parseInt(document.getElementById("loseCount").innerHTML,10)+1;
