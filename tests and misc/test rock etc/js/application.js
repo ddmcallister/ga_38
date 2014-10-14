b5a73f98bab65f0731658adee2fb914e342289f1
ddmcallister <destanie@gmail.com>
2014-10-13 22:39:44 -0400

    basic html&css final project

---
+++ tests and misc/test rock etc/js/application.js
@@ -0,0 +1,135 @@
+//main game
+var num = 0;
+function randomNum() {
+	num = Math.floor(Math.random() * 10);
+	if(num==0) {
+		var ridZero = Math.floor(Math.random() * 10);
+		num = ridZero;
+	}
+}
+//I had talked to Zachary about just adding one instead
+//of the second function in the random number generator
+//below, but I wanted a number range divisible by 3
+
+function numTranslate() {
+	if(num > 0 && num < 4) {
+		num = "rock";
+	} else if (num > 3 && num < 7) {
+		num = "paper"; 
+	} else if (num > 6 && num <10) {
+		num = "scissors";
+	}
+}
+
+var plChoice = 0;
+var notice = 0;
+var compChoice = 0;
+var userWins = ["paperrock", "rockscissors", "scissorspaper"];
+
+var scoreCompare = function() {
+    		var newPlChoice = plChoice + compChoice;
+    		notice = "newLose";
+			if (plChoice == compChoice) {
+				notice = "newTie";
+			} else {
+				$.each(userWins, function(index, value) {
+					if(newPlChoice == value) {
+					notice = "newWin";
+					}	
+				});
+			}
+		 	if(notice == "newWin") {
+				winConfirm();
+			} else if (notice == "newLose") {
+				loseConfirm();
+			} else {
+				tieConfirm();
+		}
+		}
+
+function plRock() {
+	    plChoice = "rock";
+	    randomNum();
+		numTranslate();
+		compChoice = num;
+		scoreCompare();
+}
+
+function plPaper() {
+	    plChoice = "paper";
+	    randomNum();
+		numTranslate();
+		compChoice = num;
+		scoreCompare();
+};
+function plScissors() {
+	    plChoice = "scissors";
+	   	randomNum();
+		numTranslate();
+		compChoice = num;
+		scoreCompare();
+};
+
+//kitten mode
+var kittenMode = function() {
+
+	document.getElementById("kitten_button").style.opacity = 1;
+
+	$(document).ready(function(){
+	});
+
+	$(document).keydown(function(event) {
+		kittenMode = 0;
+
+    	var code = (event.keyCode || event.which);
+    	if(code == 13) {
+        	event.preventDefault();
+    	}
+    	//above is so you can hit return on confirm instead of  
+    	//having to select OK(it takes return out of the "any 
+    	//button" group)
+
+    	randomNum();
+    	numTranslate();
+		plChoice = num;
+
+    	randomNum();
+    	numTranslate();
+		compChoice = num;
+
+		var newPlChoice = plChoice + compChoice;
+
+		scoreCompare();
+	});
+}
+
+function winConfirm() {
+	var winResponse = confirm("You win! Record your score?");
+	if (winResponse === true) {	
+  		document.getElementById("winCount").innerHTML=
+  		parseInt(document.getElementById("winCount").innerHTML,10)+1;
+	} 
+}
+
+function loseConfirm() {
+	var loseResponse = confirm("You lose! Record your score?");
+	if (loseResponse === true) {
+  		document.getElementById("loseCount").innerHTML=
+  		parseInt(document.getElementById("loseCount").innerHTML,10)+1;
+	}
+}
+
+function tieConfirm() {
+	var tieResponse = confirm("Tie! Record your score?");
+	if (tieResponse === true) {
+  		document.getElementById("tieCount").innerHTML=
+  		parseInt(document.getElementById("tieCount").innerHTML,10)+1;
+  	}
+}
+
+//score reset
+var zeroScore = function() {
+	document.getElementById("loseCount").innerHTML=0;
+	document.getElementById("tieCount").innerHTML=0;
+	document.getElementById("winCount").innerHTML=0;
+}
