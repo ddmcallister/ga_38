b39749f8123952a7a825c840442dae9782eb6812
ddmcallister <destanie@gmail.com>
2014-10-18 23:47:16 -0400

    final project rt got to title, starting gracenote

--- GA_final_project/js/application.js
+++ GA_final_project/js/application.js
@@ -38,28 +38,87 @@ navigator.geolocation.getCurrentPosition(success);
 var sigA = "r6kafxx75q3havh86syn8t4j";
 var sigB = "EHgUzuP2qj";
 
-var preSig = sigA + sigB + Date.UTL();
+var utcDate = Math.floor(Date.now() / 1000);
 
-var sha = $.sjcl.hash.sha256(preSig); {
-  console.log(preSig);
-}*/
+console.log(utcDate);
+
+var preSig = sigA + sigB + utcDate;
+
+console.log(preSig);
+
+var sha = sha256.hash(preSig)
+
+console.log(sha);
+
+function converter(bytes) {
+  for (var hex = [], i = 0; i < bytes.length; i++) {
+    hex.push((bytes[i] >>> 4).toString(16));
+    hex.push((bytes[i] & 0xF).toString(16));
+  }
+  return hex.join("");
+}
+
+var hex = converter(sha);
+console.log(hex);
+*/
+
+var movArray = 0;
+var movTimesArray = 0;
 
 
-function halfHour () {
+  function halfHour () {
 
   var movies = $.ajax({
-    url: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=d8sjemgp8m5mfpyam3cw5ea5",
+    url: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=d8sjemgp8m5mfpyam3cw5ea5",
     dataType: 'jsonp',
     success: function(jsonp) {
-      console.dir("success");
-      /*$(jsonp).appendTo(".result1");*/
+      //console.dir(movies.responseJSON.movies);
+      movArray = movies.responseJSON.movies;
+      console.log(movArray);
+      //$each(movArray function(index, value)
+          //{
+
+
+          //}
+        
+
     },
+
+
+
+    //return movArray;
+    //error: function(e) {
+      //console.log(e.message);
+    //}
+
+    //console.log(newMovArray);
+
+  });
+
+  var movieTimes = $.ajax({
+    url: "http://data.tmsapi.com/v1/movies/showings?startDate=2014-10-19&zip=10128&api_key=sjesnpx2uhtyac5frfhzfedb",
+    dataType: 'json',
+    success: function(json) {
+      console.dir("success");
+      console.dir(movies);
+      //tried movies.showings & movies.showings.10128 & movies.movies & movies[] for above
+      //movTimesArray = 
+    },
+
     error: function(e) {
       console.log(e.message);
     }
 
-  });
 
-  console.log(movies);
+  //document.getElementById("result1").innerHTML = movies;
+ });
+      console.log(movieTimes);
+
+//json object returned above is movies: Array[10] 
+//I want the following for each indexed movie 
+//id: "somenumber" - for addicted rt: "771364788", alternate id imdb: "2205401"
+/// for addicted gracenote: "tmsId": "MV005151980000", "rootId": "10233171",
+//omdb uses imdb id and has metacritic score
+//ratings - which is an object - from that object I want critics_score: somenumber
+//title: "somestring"
 }
-
