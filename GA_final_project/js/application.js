c46b6b74b343111e9e1b33afdebabb08cc5ff44f
ddmcallister <destanie@gmail.com>
2014-10-21 11:40:35 -0400

    some matching rt and onconnect, need times filter

--- GA_final_project/js/application.js
+++ GA_final_project/js/application.js
@@ -30,56 +30,104 @@ var movArray = 0;
 var movTimesArray = [];
 
 
+
 var d = new Date();
 var y = d.getFullYear();
 var m =  d.getMonth() + 1;
 var dd = d.getDate();
 var h = d.getHours();
 var mi = d.getMinutes();
+
 var dateString = y + "-" + m + "-" + dd + "T" + h + ":" + mi;
 
-  function halfHour () {
+var keyDateString = y + "-" + m + "-" + dd;
+var urlDate = "http://data.tmsapi.com/v1/movies/showings?startDate=" + keyDateString + "&zip=10128&api_key=sjesnpx2uhtyac5frfhzfedb";
 
-  var movies = $.ajax({
-    url: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=d8sjemgp8m5mfpyam3cw5ea5",
+function halfHour () {
+
+/*  var movies = $.ajax({
+    url: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=d8sjemgp8m5mfpyam3cw5ea5&page_limit=50",
     dataType: 'jsonp',
     success: function(jsonp) {
-      console.dir(movies.responseJSON.movies);
       movArray = movies.responseJSON.movies;
-      console.log(movArray[2].ratings.critics_score);
-      for(var rtMovArray = [], i=0; i<movArray.length; i++) {
-
-        rtMovArray.push(movArray[i].title, movArray[i].ratings.critics_score);
-        //movArray[i].links.alternate is links to reviews
-        document.getElementById("result1").innerHTML = rtMovArray;
+      for(rtTitlArr = [], i=0; i<movArray.length; i++) {
+        rtTitlArr.push([movArray[i].title, movArray[i].ratings.critics_score]);
       }
+
+      console.log(rtTitlArr);
+     
+        //movArray[i].links.alternate is links to reviews
+        //document.getElementById("result1").innerHTML = globMovArr;
 
     //error: function(e) {
       //console.log(e.message);
     
     }
+  });*/
 
-  });
+
 
   var movieTimes = $.ajax({
-    url: "http://data.tmsapi.com/v1/movies/showings?startDate=2014-10-20&zip=10128&api_key=sjesnpx2uhtyac5frfhzfedb",
+    url: urlDate,
+      //"http://data.tmsapi.com/v1/movies/showings?startDate=2014-10-20&zip=10128&api_key=sjesnpx2uhtyac5frfhzfedb",
         //http://data.tmsapi.com/v1/movies/showings?startDate=2014-10-19&lat=41&lng=-74&api_key=sjesnpx2uhtyac5frfhzfedb
     dataType: 'json',
     success: function(json) {
-      console.dir("success");
-      console.dir(movieTimes.responseJSON[1].showtimes);
       movTimesArray = movieTimes.responseJSON;
-      console.dir(movTimesArray[1].showtimes)
-      for(var tmsMovArray = [], i=0; i<movTimesArray.length; i++) {
-        tmsMovArray.push(movTimesArray[i].title, movTimesArray[i].showtimes);
+      for(var globMovTimes = [], i=0; i<movTimesArray.length; i++) {
+        globMovTimes.push([movTimesArray[i].title, movTimesArray[i].showtimes]); 
       //displays dateTime: "2014-10-20T16:45" 
       }
+
+    console.log(globMovTimes[0][0]);
+
+    var movies = $.ajax({
+    url: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=d8sjemgp8m5mfpyam3cw5ea5&page_limit=50",
+    dataType: 'jsonp',
+    success: function(jsonp) {
+      movArray = movies.responseJSON.movies;
+      for(rtTitlArr = [], i=0; i<movArray.length; i++) {
+        rtTitlArr.push([movArray[i].title, movArray[i].ratings.critics_score]);
+      }
+
+      //var comTitlArr = [];
+
+      /*$each(globMovTimes, function() { 
+          if (globMovTimes[0][0] == rtTitlArr[0][0]) {
+           comTitlArr.push([rtTitlArr[0][1], globMovArr[1]]); 
+          }
+
+        });*/
+
+      for(comTitlArr = [], i=0; i<globMovTimes.length; i++) {
+        for(j=0; j<rtTitlArr.length; j++) {
+          if (globMovTimes[i][0] == rtTitlArr[j][0]) {
+           comTitlArr.push([rtTitlArr[j][0], rtTitlArr[j][1], globMovTimes[i][1]]);
+         }
+       }
+      }
+
+      console.log(rtTitlArr[0][0]);
+      console.log(comTitlArr);
+     
+        //movArray[i].links.alternate is links to reviews
+      }
+
+
+
+  });
+
     },
 
     error: function(e) {
       console.log(e.message);
     }
 
+
  });
 
+  //console.log(globMovTimes);
+
 }
+
+//document.getElementById("result1").innerHTML = globMovArr;
